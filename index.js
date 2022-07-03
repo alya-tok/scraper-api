b16bd886d58d47d261d7bce594d4817653c422b7
alya-tok <apriliaez46@gmail.com>
2022-07-03 13:15:37 +0700

    scraper-api

---
+++ index.js
@@ -0,0 +1,8 @@
+this.search = require('./lib/search')
+this.downloader = require('./lib/downloader')
+this.info = require('./lib/information')
+this.primbon = require('./lib/primbon')
+this.anime = require('./lib/anime')
+this.religi = require('./lib/religion')
+global.creator = '@Liya'
+module.exports = this
